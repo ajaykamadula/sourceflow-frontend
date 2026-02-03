7bde53d6ec6e89802af0a658d367cb126b9c7904
ajaykamadula <kamadulaajaykumar@gmail.com>
2026-02-03 08:30:06 +0530

    changed repoissue.css

--- src/components/issue/RepoIssues.jsx
+++ src/components/issue/RepoIssues.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Navbar from "../Navbar.jsx";
-import "./repoIssues.css";
+import "./repoissues.css";
 
 const RepoIssues = () => {
   const { id } = useParams(); // repoId
